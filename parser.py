acb55c0d80aaba4f7d0036f3b19c2511ef3a1b36
Derek Willis <dwillis@gmail.com>
2015-09-10 22:04:24 -0400

    updated parser

--- parser.py
+++ parser.py
@@ -3,19 +3,20 @@ import unicodecsv
 headers = ['county', 'office', 'district', 'party', 'candidate', 'votes', 'winner', 'write-in', 'notes']
 counties = ['Apache','Cochise','Coconino','Gila','Graham','Greenlee','La Paz','Maricopa','Mohave','Navajo','Pima','Pinal','Santa Cruz','Yavapai','Yuma','']
 cds = ['U.S. House 1', 'U.S. House 2', 'U.S. House 3', 'U.S. House 4', 'U.S. House 5', 'U.S. House 6', 'U.S. House 7', 'U.S. House 8', '']
-offices = ['PRESIDENT OF THE UNITED STATES', 'U.S. SENATOR', 'U.S. REPRESENTATIVE IN CONGRESS', 'GOVERNOR', 'STATE SENATOR', 'STATE REPRESENTATIVE', 'SECRETARY OF STATE', 'ATTORNEY GENERAL', 'STATE TREASURER', 'SUPERINTENDENT OF PUBLIC INSTRUCTION', 'STATE MINE INSPECTOR', 'CORPORATION COMMISSIONER']
+offices = ['PRESIDENT OF THE UNITED STATES', 'U.S. SENATOR', 'U.S. REPRESENTATIVE IN CONGRESS', 'GOVERNOR', 'STATE SENATOR', 'STATE REPRESENTATIVE', 'SECRETARY OF STATE', 'ATTORNEY GENERAL', 'STATE TREASURER', 'SUPERINTENDENT OF PUBLIC INSTRUCTION', 'STATE MINE INSPECTOR', 'CORPORATION COMMISSIONER', 'CORPORATION COMMISSION', 'PRESIDENTIAL ELECTORS']
 office_lookup = {
     'U.S. SENATOR' : 'U.S. Senate', 'U.S. REPRESENTATIVE IN CONGRESS' : 'U.S. House', 'GOVERNOR' : 'Governor', 'STATE SENATOR' : 'State Senate',
     'STATE REPRESENTATIVE' : 'State House', 'SECRETARY OF STATE' : 'Secretary of State', 'ATTORNEY GENERAL' : 'Attorney General',
     'STATE TREASURER' : 'State Treasurer', 'SUPERINTENDENT OF PUBLIC INSTRUCTION' : 'Superintendent of Public Instruction',
-    'STATE MINE INSPECTOR' : 'State Mine Inspector', 'CORPORATION COMMISSIONER' : 'Corporation Commissioner', 'PRESIDENT OF THE UNITED STATES' : 'President'
+    'STATE MINE INSPECTOR' : 'State Mine Inspector', 'CORPORATION COMMISSIONER' : 'Corporation Commissioner', 'PRESIDENT OF THE UNITED STATES' : 'President',
+    'CORPORATION COMMISSION' : 'Corporation Commissioner', 'PRESIDENTIAL ELECTORS' : 'President'
 }
 
-with open('20060912__az__primary.csv', 'wb') as csvfile:
+with open('20001107__az__general.csv', 'wb') as csvfile:
     w = unicodecsv.writer(csvfile, encoding='utf-8')
     w.writerow(headers)
 
-    lines = open('/Users/DW-Admin/Downloads/Canvass2006PE.txt').readlines()
+    lines = open('/Users/derekwillis/Downloads/Canvass2000GE.txt').readlines()
 
     for line in lines:
         party = None
@@ -23,7 +24,7 @@ with open('20060912__az__primary.csv', 'wb') as csvfile:
         notes = None
         if line == '\n':
             continue
-        if len([x for x in offices if x in line]) == 1:
+        if len([x for x in offices if x in line]) > 0:
             office = [x for x in offices if x in line][0]
             if 'DISTRICT' in line:
                 office, district = line.strip().split(' - ')
@@ -31,6 +32,8 @@ with open('20060912__az__primary.csv', 'wb') as csvfile:
             else:
                 district = None
             display_office = office_lookup[office]
+            if 'TERM EXPIRING' in office:
+                notes = line.strip().split(' - ')[1]
         else:
             bits = line.split('\t')
             candidate_bits = bits[0]
@@ -43,7 +46,7 @@ with open('20060912__az__primary.csv', 'wb') as csvfile:
             county_results = zip(counties, results_bits)
             county_results = [(x,y) for (x,y) in county_results if y != '']
             for county, votes in county_results:
-                if '*' in candidate_bits.split(')')[1].strip() and county == '':
+                if '*' in candidate_bits and county == '':
                     race_winner = True
                 else:
                     race_winner = None
